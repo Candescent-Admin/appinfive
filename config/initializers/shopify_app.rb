230367e9f874d49f1bf278b2bcbd4a47b2aa4c5b
Candescent-Admin <info@candescent.ca>
2016-11-23 21:41:58 -0500

    test live 2

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,6 @@
 ShopifyApp.configure do |config|
   config.api_key = "59fa2e7dbd7825dba6a1f3eec62eada6"
   config.secret = "0093968f27e819b3e1d69d0a1d0fac32"
-  config.redirect_uri = "https://powerful-tor-23639.herokuapp.com/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
